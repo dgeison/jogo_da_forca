e31175b81a66c6b3183de282cf1200a37cbd1183
dgeison <dgeison.peixoto@gmail.com>
2023-06-08 10:45:34 -0300

    updated hangman with new constants

--- hangman.py
+++ hangman.py
@@ -1,28 +1,18 @@
 import random
+from constants import WORDS_LENGTHS, DIFFICULTY_LEVELS
 
-difficulty_levels = {
-    '1': 'Fácil',
-    '2': 'Médio',
-    '3': 'Difícil'
-}
-
-words_lengths = {
-    '1': (3, 5),
-    '2': (6, 10),
-    '3': (11, 1000)
-}
 
 print("A seguir escolha um nível de dificuldade: ")
 
 difficulty_level = ''
 
 while not difficulty_level:
-    for k, v in difficulty_levels.items():
+    for k, v in DIFFICULTY_LEVELS.items():
         print(f'{k} - {v.upper()}')
 
     difficulty_level = input("Escolha um nível de dificuldade: ")
 
-    if difficulty_level not in difficulty_levels.keys():
+    if difficulty_level not in DIFFICULTY_LEVELS.keys():
         print("Nível inválido, tente novamente!")
         difficulty_level = ''
 
@@ -31,7 +21,7 @@ with open('static/words.txt', mode='r') as f_words:
     words = []
     for word in f_words.readlines():
         w = word.strip()
-        min, max = words_lengths[difficulty_level]
+        min, max = WORDS_LENGTHS[difficulty_level]
 
         # if len(w) >= min and len(w) =< max:
         if min <= len(w) <= max:
