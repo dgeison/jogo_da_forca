11f1da6ec2841abac93c682e59b374f8b0a99180
dgeison <dgeison.peixoto@gmail.com>
2023-06-08 11:05:44 -0300

    created three functions show, random and get

--- hangman.py
+++ hangman.py
@@ -2,44 +2,50 @@ import random
 from constants import WORDS_LENGTHS, DIFFICULTY_LEVELS
 
 
-print("A seguir escolha um nível de dificuldade: ")
+def show_difficult_menu():
+    """Show the difficult menu"""
+    print("A seguir escolha um nível de dificuldade: ")
 
-difficulty_level = ''
+    difficulty_level = ''
 
-while not difficulty_level:
-    for k, v in DIFFICULTY_LEVELS.items():
-        print(f'{k} - {v.upper()}')
+    while not difficulty_level:
+        for k, v in DIFFICULTY_LEVELS.items():
+            print(f'{k} - {v.upper()}')
+        difficulty_level = input("Escolha um nível de dificuldade: ")
 
-    difficulty_level = input("Escolha um nível de dificuldade: ")
-
-    if difficulty_level not in DIFFICULTY_LEVELS.keys():
-        print("Nível inválido, tente novamente!")
-        difficulty_level = ''
-
-
-with open('static/words.txt', mode='r') as f_words:
-    words = []
-    for word in f_words.readlines():
-        w = word.strip()
-        min, max = WORDS_LENGTHS[difficulty_level]
-
-        # if len(w) >= min and len(w) =< max:
-        if min <= len(w) <= max:
-            words.append(w)
+        if difficulty_level not in DIFFICULTY_LEVELS.items():
+            print("Nível inválido, tente novamente!")
+            difficulty_level = ''
+    return difficulty_level
 
 
-max_index = len(words) - 1
-random_index = random.randint(0, max_index)
+def get_random_word(difficulty_level):
+    """Get a random word from a file"""
+    with open('static/words.txt', mode='r', encoding='utf-8') as f_words:
+        words = []
+        for word in f_words.readlines():
+            w = word.strip()
+            min, max = WORDS_LENGTHS[difficulty_level]
 
-selected_word = words[random_index]
+            # if len(w) >= min and len(w) =< max:
+            if min <= len(w) <= max:
+                words.append(w)
 
-print(selected_word)
+    max_index = len(words) - 1
+    random_index = random.randint(0, max_index)
+
+    selected_word = words[random_index]
+
+    print(selected_word)
+    return selected_word
 
 
-total_attempts = 2 * len(selected_word)
-if difficulty_level == '1':
-    total_attempts += 2
-elif difficulty_level == '3':
-    total_attempts -= 2
+def get_total_attempts(selected_word, difficulty_level):
+    """Return the total attempts based on the difficulty level"""
+    total_attempts = 2 * len(selected_word)
+    if difficulty_level == '1':
+        total_attempts += 2
+    elif difficulty_level == '3':
+        total_attempts -= 2
 
-print(f'Você tem {total_attempts} tentativas!')
+    print(f'Você tem {total_attempts} tentativas!')
