ca5f278d0afdb9b07361a4e18819a2c7321dc4c9
dgeison <dgeison.peixoto@gmail.com>
2023-06-11 19:53:44 -0300

    function improvement with list comprehension

--- hangman.py
+++ hangman.py
@@ -1,49 +1,19 @@
-import re
-from utils import show_difficult_menu, get_random_word, get_total_attempts
+from utils import show_difficult_menu
+from utils import get_random_word
+from utils import play_hangman
 
 
-difficulty_level = show_difficult_menu()
-selected_word = get_random_word(difficulty_level=difficulty_level)
-total_attempts = get_total_attempts(selected_word, difficulty_level)
-available_attempts = total_attempts
-
-print(f"A quantidade de chances para acertar a palavra é: {total_attempts}")
-
-current_state = ["_" for letter in selected_word]
-guessed_letters = []
-
-while "_" in current_state and available_attempts:
-    print(
-        f"\n\n### Tentativa número: {total_attempts - available_attempts + 1} de {total_attempts}###"
+if __name__ == "__main__":
+    difficulty_level = show_difficult_menu()
+    selected_word = get_random_word(difficulty_level=difficulty_level)
+    player_wins = play_hangman(
+        selected_word=selected_word, difficulty_level=difficulty_level
     )
-    for char in current_state:
-        print(char, end=" ")
 
-    guess = ""
 
-    while not guess:
-        guess = input("\nDigite uma letra: ").lower()
-        if len(guess) != 1 and not re.match("[a-z]", guess):
-            print("Entrada inválida. Tente novamente, entrando apenas 1 letra.")
-            guess = ""
-
-    if guess not in guessed_letters:
-        guessed_letters.append(guess)
-
-        if guess in selected_word:
-            pass
-        positions = []
-        index = 0
-        for letter in selected_word:
-            if letter == guess:
-                positions.append(index)
-            index += 1
-
-        for index in positions:
-            current_state[index] = guess
-
-        else:
-            available_attempts -= 1
-
+    if player_wins:
+        print("Parabéns, você venceu!")
     else:
-        print("Você já tentou essa letra!")
+        print("Que pena, você perdeu!")
+
+    print(f"A palavra era: {selected_word}")
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import re
+
 import random
 from constants import WORDS_LENGTHS, DIFFICULTY_LEVELS
 
@@ -47,7 +49,6 @@ def get_random_word(difficulty_level):
     random_index = random.randint(0, max_index)
 
     selected_word = words[random_index]
-    print(f"A palavra selecionada foi: {selected_word}")
     return selected_word
 
 
@@ -70,3 +71,51 @@ def get_total_attempts(selected_word, difficulty_level):
 
     total_attempts = round(total_attempts)
     return total_attempts
+
+
+def play_hangman(selected_word, difficulty_level):
+    """
+    Com base em uma palavra que deve ser advinhada e um niível de dificuldade
+    interage com um usuario para simular um "jogo da forca".
+
+    :param selected_word: string que representa a palavra a ser advinhada
+    :param difficulty_setting: string que representa a dificuldade do jogo
+    :return: int que se positivo significa que o jogador advinhou a palavra corretamente.
+    Caso contrário, o jogador perdeu.
+    """
+    total_attempts = get_total_attempts(selected_word, difficulty_level)
+    available_attempts = total_attempts
+    current_state = ["_" for letter in selected_word]
+    guessed_letters = []
+
+    while "_" in current_state and available_attempts:
+        print(
+            f"\n\n### Tentativa número: {total_attempts - available_attempts + 1} de {total_attempts}###"
+        )
+        for char in current_state:
+            print(char, end=" ")
+
+        guess = ""
+
+        while not guess:
+            guess = input("\nDigite uma letra: ").lower()
+            if len(guess) != 1 or not re.match("[a-z]", guess):
+                print("Entrada inválida. Tente novamente, entrando apenas 1 letra.")
+                guess = ""
+
+        if guess not in guessed_letters:
+            guessed_letters.append(guess)
+
+            if guess in selected_word:
+                positions = [m.start() for m in re.finditer(guess, selected_word)]
+
+                for index in positions:
+                    current_state[index] = guess
+
+            else:
+                available_attempts -= 1
+
+        else:
+            print("Você já tentou essa letra!")
+
+    return available_attempts
